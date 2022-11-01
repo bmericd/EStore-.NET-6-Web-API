2859b1d6f8e0e5e0dddcc4a6afa71f02ecf1a8c7
Buğra Meriç Değirmenci <45015835+bmericd@users.noreply.github.com>
2022-11-01 19:04:43 +0300

    First Commit

---
+++ EStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            
+            return Ok(await _context.Products.ToListAsync()); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+                return BadRequest("Product not found.");
+            return Ok(product); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            
+             _context.Products.AddAsync(product);
+             await _context.SaveChangesAsync();
+            
+            return Ok(await _context.Products.ToListAsync()); 
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product updatedProduct)
+        {
+            
+            var dbProduct = await _context.Products.FindAsync(updatedProduct.Id);
+            if(dbProduct == null)
+                return BadRequest("Product not found.");
+
+            dbProduct.Id = updatedProduct.Id;
+            dbProduct.Name = updatedProduct.Name;
+            dbProduct.Price = updatedProduct.Price;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Products.ToListAsync()); 
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Product>>> DeleteProduct(int id)
+        {
+            var dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null)
+                return BadRequest("Product not found.");
+
+            _context.Products.Remove(dbProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Products.ToListAsync());
+        }
+        
+    }
+}
---
+++ EStoreAPI/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace EStoreAPI
+{
+    public class Product
+    {
+        public int Id {get; set; }
+        public string Name { get; set; } = String.Empty;
+        public float Price { get; set; } = 0.0f;
+    }
+}
